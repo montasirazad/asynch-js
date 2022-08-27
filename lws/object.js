68a05da3347ed6ac3f2ef27232bee1669e122421
montasirazad <montasirazad@gmail.com>
2022-08-27 17:45:21 +0600

    wba

--- lws/object.js
+++ lws/object.js
@@ -1,12 +1,21 @@
 const car = {
     name: 'fiat',
-    model: '55',
+    model: '55YLD',
     weight: '1000 kg',
     color: 'white',
     start: function () {
         console.log('car has started');
+        this.drive()
     },
     drive: function () {
         console.log('car is driving');
     }
 }
+
+console.log(car.color);
+console.log(car['model']);
+car.start()
+
+
+let x = new Number(5)
+console.log(typeof (x));
